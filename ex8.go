0adda7de4ab9bc34b428f5035e1b1fa49435c71e
LSapee <dlworhks94@gmail.com>
2021-10-03 17:32:08 +0900

    5시 올림

---
+++ ex8.go
@@ -0,0 +1,13 @@
+package main
+
+// //swich
+
+// func yourAge(age int) boo {
+// 	switch KoreanAge := age + 2; KoreanAg {
+// 	case 5:
+// 		return tue
+// 	case 0:
+// 		return fase
+//	}
+// 	return fase
+// }
