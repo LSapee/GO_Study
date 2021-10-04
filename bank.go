581bcead75d4c7dea5e2ca9a7ffb346b4f076221
Sapee <dlworhks94@gmail.com>
2021-10-04 20:01:03 +0900

    test

--- bank.go
+++ bank.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
-	"goproject/git_try/banking"
+
+	/*
+		외부 저장소 가져올 때는 go mod tidy를 써서 필요한 외부 저장소를 받아준다
+		1.import 에 외부 저장소 주소를 적고
+		2.터미널에 go mod tidy
+		3. 확실하게 인포트 되면 import주소의 빨간줄이 없어진다.
+	*/
+	"github.com/Lsapee/GO_Study/banking"
 )
 
 func main() {
