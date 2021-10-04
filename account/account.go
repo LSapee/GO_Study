e1d1711c283ee1c042c24be43cff81f226743106
Sapee <dlworhks94@gmail.com>
2021-10-04 21:56:54 +0900

    bank -> account

---
+++ account/account.go
@@ -0,0 +1,12 @@
+package account
+
+//Account struct
+type Account struct {
+	owner   string
+	balance int
+}
+
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
