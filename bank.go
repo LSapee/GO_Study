a16290ede9974e3a0b57fcac4bab55e0e2f985ce
LSapee <dlworhks94@gmail.com>
2021-10-04 16:52:19 +0900

    일단 서브 올리자

---
+++ bank.go
@@ -0,0 +1,6 @@
+package main
+
+
+func main() {
+	account := banking.Account(owner : "jk" , balance : 13000)
+}
--- ex11.go
+++ ex11.go
@@ -1,3 +1,25 @@
 package main
 
-//map
+// import "fmt"
+
+// //구조체 strct
+// type name struc {
+// 	name strng
+// 	age  nt
+// 	car  bol
+// }
+
+// func main( {
+// 	//map =  mapname := map[keytype]valuetype{key:value,..}
+// /*
+// 		jk := map[string]string{"name": "jk", "age": "1"}
+// 		for key, value := range j {
+// 			fmt.Println(key, vale)
+// 	}
+// 		fmt.Println(k)
+// */
+
+// 	jk := name{"jk", 28, fale}
+// 	fmt.Println(jk.ae)
+
+// }
