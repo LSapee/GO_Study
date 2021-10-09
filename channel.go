18ee9ee7b516f49bde88173a85704e688ea651e8
Sapee <dlworhks94@gmail.com>
2021-10-09 12:49:21 +0900

    채널 만들기

---
+++ channel.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
+
+//기본 URL
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+
+//실행 함수
+func main() {
+	var jobs []extractedJob
+	// 전체 페이지 수 만큼 불러오기
+	totalPages := getPages()
+	// fmt.Println(totalPages)
+
+	for i := 0; i < totalPages; i++ {
+		//전체 페이지수 만큼 반복해서 페이지 불러오기
+		extractedJob := getPage(i)
+		//배열 합칠때 뒤쪽에 ...달아주기
+		jobs = append(jobs, extractedJob...)
+	}
+	writeJobs(jobs)
+	fmt.Println("Done, extraced", len(jobs))
+}
+
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"ID", "Title", "Location", "salary", "Summary"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
+}
+
+func getPage(page int) []extractedJob {
+	c := make(chan extractedJob)
+	var jobs []extractedJob
+	// page당 url 변경 되는것
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	// 처음 페이지 URL 출력
+	fmt.Println("Requesting", pageURL)
+	// 페이지 변수 넣기
+	res, err := http.Get(pageURL)
+	// 에러 발생시 에러 소환 입력값 err
+	checkErr(err)
+	// 정상 작동시 checkCode 작동 입력값 res
+	checkCode(res)
+	// 에러 발생해도 나중에 함수를 끝내기 위해 close 마지막에 실행
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	//탐색할 html element  선택자는 css 선택자로
+	searchCards := doc.Find(".tapItem")
+	//탐색하기
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		go extractJob(card, c)
+	})
+	for i := 0; i < searchCards.Length(); i++ {
+		job := <-c
+		jobs = append(jobs, job)
+	}
+	return jobs
+}
+
+func extractJob(card *goquery.Selection, c chan<- extractedJob) {
+	id, _ := card.Attr("id")
+	id = strings.TrimLeft(id, "job_")
+	title := cleanString(card.Find("h2>span").Text())
+	location := cleanString(card.Find("div pre").Text())
+	salary := cleanString(card.Find(".salary-snippet").Text())
+	summary := cleanString(card.Find(".job-snippet").Text())
+	c <- extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary}
+}
+
+//페이지 가져오기 URL
+func getPages() int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+
+		// fmt.Println(s.Find("a").Length())
+	})
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status: ", res.StatusCode)
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
--- getPages.go
+++ getPages.go
@@ -1,113 +1,137 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"strconv"
-	"strings"
+// import (
+// 	"encoding/csv"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// 	"strconv"
+// 	"strings"
 
-	"github.com/PuerkitoBio/goquery"
-)
+// 	"github.com/PuerkitoBio/goquery"
+// )
 
-type extractedJob struct {
-	id       string
-	title    string
-	location string
-	salary   string
-	summary  string
-}
+// type extractedJob struct {
+// 	id       string
+// 	title    string
+// 	location string
+// 	salary   string
+// 	summary  string
+// }
 
-//기본 URL
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+// //기본 URL
+// var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
-//실행 함수
-func main() {
-	var jobs []extractedJob
-	// 전체 페이지 수 만큼 불러오기
-	totalPages := getPages()
-	// fmt.Println(totalPages)
+// //실행 함수
+// func main() {
+// 	var jobs []extractedJob
+// 	// 전체 페이지 수 만큼 불러오기
+// 	totalPages := getPages()
+// 	// fmt.Println(totalPages)
 
-	for i := 0; i < totalPages; i++ {
-		//전체 페이지수 만큼 반복해서 페이지 불러오기
-		extractedJob := getPage(i)
-		jobs = append(jobs, extractedJob...)
-	}
-	fmt.Println(jobs)
-}
+// 	for i := 0; i < totalPages; i++ {
+// 		//전체 페이지수 만큼 반복해서 페이지 불러오기
+// 		extractedJob := getPage(i)
+// 		//배열 합칠때 뒤쪽에 ...달아주기
+// 		jobs = append(jobs, extractedJob...)
+// 	}
+// 	writeJobs(jobs)
+// 	fmt.Println("Done, extraced", len(jobs))
+// }
 
-func getPage(page int) []extractedJob {
-	var jobs []extractedJob
-	// page당 url 변경 되는것
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
-	// 처음 페이지 URL 출력
-	fmt.Println("Requesting", pageURL)
-	// 페이지 변수 넣기
-	res, err := http.Get(pageURL)
-	// 에러 발생시 에러 소환 입력값 err
-	checkErr(err)
-	// 정상 작동시 checkCode 작동 입력값 res
-	checkCode(res)
-	// 에러 발생해도 나중에 함수를 끝내기 위해 close 마지막에 실행
-	defer res.Body.Close()
+// func writeJobs(jobs []extractedJob) {
+// 	file, err := os.Create("jobs.csv")
+// 	checkErr(err)
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	checkErr(err)
-	//탐색할 html element  선택자는 css 선택자로
-	searchCards := doc.Find(".tapItem")
-	//탐색하기
-	searchCards.Each(func(i int, card *goquery.Selection) {
-		job := extractJob(card)
-		jobs = append(jobs, job)
-	})
-	return jobs
-}
+// 	w := csv.NewWriter(file)
+// 	defer w.Flush()
 
-//페이지 가져오기 URL
-func getPages() int {
-	pages := 0
-	res, err := http.Get(baseURL)
-	checkErr(err)
-	checkCode(res)
+// 	headers := []string{"ID", "Title", "Location", "salary", "Summary"}
 
-	defer res.Body.Close()
+// 	wErr := w.Write(headers)
+// 	checkErr(wErr)
 
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	checkErr(err)
-	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-		pages = s.Find("a").Length()
+// 	for _, job := range jobs {
+// 		jobSlice := []string{"https://kr.indeed.com/viewjob?jk=" + job.id, job.title, job.location, job.salary, job.summary}
+// 		jwErr := w.Write(jobSlice)
+// 		checkErr(jwErr)
+// 	}
+// }
 
-		// fmt.Println(s.Find("a").Length())
-	})
-	return pages
-}
+// func getPage(page int) []extractedJob {
+// 	var jobs []extractedJob
+// 	// page당 url 변경 되는것
+// 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+// 	// 처음 페이지 URL 출력
+// 	fmt.Println("Requesting", pageURL)
+// 	// 페이지 변수 넣기
+// 	res, err := http.Get(pageURL)
+// 	// 에러 발생시 에러 소환 입력값 err
+// 	checkErr(err)
+// 	// 정상 작동시 checkCode 작동 입력값 res
+// 	checkCode(res)
+// 	// 에러 발생해도 나중에 함수를 끝내기 위해 close 마지막에 실행
+// 	defer res.Body.Close()
 
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
+// 	doc, err := goquery.NewDocumentFromReader(res.Body)
+// 	checkErr(err)
+// 	//탐색할 html element  선택자는 css 선택자로
+// 	searchCards := doc.Find(".tapItem")
+// 	//탐색하기
+// 	searchCards.Each(func(i int, card *goquery.Selection) {
+// 		job := extractJob(card)
+// 		jobs = append(jobs, job)
+// 	})
+// 	return jobs
+// }
 
-func checkCode(res *http.Response) {
-	if res.StatusCode != 200 {
-		log.Fatalln("Request failed with Status: ", res.StatusCode)
-	}
-}
+// //페이지 가져오기 URL
+// func getPages() int {
+// 	pages := 0
+// 	res, err := http.Get(baseURL)
+// 	checkErr(err)
+// 	checkCode(res)
 
-func extractJob(card *goquery.Selection) extractedJob {
-	id, _ := card.Attr("id")
-	title := cleanString(card.Find("h2>span").Text())
-	location := cleanString(card.Find("div pre").Text())
-	salary := cleanString(card.Find(".salary-snippet").Text())
-	summary := cleanString(card.Find(".job-snippet").Text())
-	return extractedJob{
-		id:       id,
-		title:    title,
-		location: location,
-		salary:   salary,
-		summary:  summary}
-}
+// 	defer res.Body.Close()
 
-func cleanString(str string) string {
-	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
-}
+// 	doc, err := goquery.NewDocumentFromReader(res.Body)
+// 	checkErr(err)
+// 	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+// 		pages = s.Find("a").Length()
+
+// 		// fmt.Println(s.Find("a").Length())
+// 	})
+// 	return pages
+// }
+
+// func checkErr(err error) {
+// 	if err != nil {
+// 		log.Fatalln(err)
+// 	}
+// }
+
+// func checkCode(res *http.Response) {
+// 	if res.StatusCode != 200 {
+// 		log.Fatalln("Request failed with Status: ", res.StatusCode)
+// 	}
+// }
+
+// func extractJob(card *goquery.Selection) extractedJob {
+// 	id, _ := card.Attr("id")
+// 	id = strings.TrimLeft(id, "job_")
+// 	title := cleanString(card.Find("h2>span").Text())
+// 	location := cleanString(card.Find("div pre").Text())
+// 	salary := cleanString(card.Find(".salary-snippet").Text())
+// 	summary := cleanString(card.Find(".job-snippet").Text())
+// 	return extractedJob{
+// 		id:       id,
+// 		title:    title,
+// 		location: location,
+// 		salary:   salary,
+// 		summary:  summary}
+// }
+
+// func cleanString(str string) string {
+// 	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+// }
