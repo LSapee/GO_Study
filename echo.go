7d61b2ab37af472c40c00ab9d2af9d0f0bafcb96
Sapee <dlworhks94@gmail.com>
2021-10-10 19:34:38 +0900

    echo

---
+++ echo.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"goproject/Nomad_Coders/scrapper"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+const file_NAME string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove(file_NAME)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(file_NAME, file_NAME)
+}
+func main() {
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":8080"))
+}
